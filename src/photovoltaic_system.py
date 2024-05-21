5c19832f69062a4a6437c25861afda4bc58fd512
FRTNX <frtnx@protonmail.com>
2024-05-21 15:00:29 +0200

    Completed simulated time mechanics

--- src/photovoltaic_system.py
+++ src/photovoltaic_system.py
@@ -1,7 +1,11 @@
 from typing import List, Tuple, Union
 from simulator_types import Celcius, Percentage, Watt, KiloWatt, Volt
 
+from datetime import datetime
+from freezegun import freeze_time
+
 import time
+import json
 import threading
 
 
@@ -10,7 +14,7 @@ class Environment:
     
     def __init__(self):
         """Initialise environment in 'frozen' state."""
-        self._time = Time()
+        self._datetime = ''
         self._active = False
         self._update_interval = 1
         self._max_solar_irradiance: Watt = 1000
@@ -21,7 +25,7 @@ class Environment:
     @property
     def current_time(self):
         """Time adapter function."""
-        return self._time.get_time()
+        return str(self._datetime)
     
     @property
     def zenith_time(self):
@@ -38,72 +42,54 @@ class Environment:
     
     def solar_irradiance(self):
         """Calculate current solar irradiance with respect to time."""
-        loss = abs(self.zenith_time - self.current_time)
+        loss = abs(self.zenith_time - self._integer_time(self._datetime))
         irradiance = self.max_solar_irradiance - loss
         return irradiance if irradiance > 0 else 0
         
-    def start(self):
-        """Start space-time.""" 
-        self._active = True
-        update_thread = threading.Thread(target=self._update)
-        update_thread.start()
-        
-    def stop(self):
-        """Stop space-time."""
-        self._active = False
-        
-    def _update(self):
-        while self._active:
-            self._cycle()
-            time.sleep(self._update_interval)
-    
-    def _cycle(self):
-        """Change state by one cycle."""
-        self._time.tick()
-
-# todo: improve to work well in time-series analysis
-class Time:
-    """Simulate time."""
-    
-    def __init__(self, initial_time: int=0):
-        self._time = initial_time
-        
-    def get_time(self):
-        """Return current time."""
-        return self._time
-    
-    def tick(self, value: int=1):
-        """Increment time by value."""
-        if self._time < 2359:                       # 24-hour format
-            self._time += value
-        else:
-            self._time = 0                          # reset
+    def set_time(self, simulated_time: datetime):
+        self._datetime = simulated_time
+            
+    def _integer_time(self, date_time: datetime):
+        """Return time as an integer for mock solar irradiance calculations."""
+        try:
+            int_time = int(date_time.strftime('%X').replace(':', '')[:4])
+            return int_time
+        except:
+            return 0
 
         
 class Battery:
     """A battery class."""
     
-    def __init__(self, voltage: int = 12, amps: int = 50):
+    def __init__(self, capacity: int = 12, amps: int = 50):
         self._id = ''           # todo: generate unique id
-        self._voltage = 12
-        self._amperes = amps
-        self._available_volts = 12.2
-        self._solar_panel: SolarArray = None  
-        self._charge_controller = None
-        self._time_series = []
+        self._capacity: KiloWatt = capacity
+        self._state_of_charge: Percentage = 0.95
+        self._max_charge_rate: KiloWatt = 0
+        self._max_discharge_rate: KiloWatt = 0
+        self._depth_of_charge: Percentage = 0.50
+        self._amperes: int = amps
+        self._available_power = self._capacity * self._state_of_charge
 
-    def charge(self, watts: int):
+    def charge(self, power: KiloWatt):
         """Add to available volts."""
-        if self._solar_panel:
-            watts = self._solar_panel.discharge()
+        if power > self._max_charge_rate:
+            power = self._max_charge_rate     # limit power to max rate
+        if self._available_power + power <= self._capacity:
+            self._available_power += power
+        self._state_of_charge = self._available_power / self._capacity   
         
-    def discharge(self, volts):
+    def discharge(self, power: KiloWatt):
         """"""
         
     def status(self):
+        available_power = self._capacity * self._state_of_charge
         return {
             'battery_id': self._id,
-            'volts': self._available_volts
+            'capacity': self._capacity,
+            'state_of_charge': self._state_of_charge,
+            'available_power': { 'unit': 'kilowatt', 'value': available_power },
+            'voltage': self._available_power / self._amperes
         }
     
 
@@ -112,7 +98,7 @@ class BatteryArray:
     
     def __init__(self):
         """"""
-        self._batteries = []
+        self._battery_array = []
         self._capacity: Volt = 0
         self._voltage: Volt = 0
         self._connection_type = None
@@ -120,31 +106,33 @@ class BatteryArray:
         
     def add(self, battery: Battery, connection_type: str='series'):
         """Connect a new battery."""
-        # todo: validate compatability
-        self._batteries.append(battery)
+        # todo: validate battery compatability
+        self._battery_array.append(battery)
         return { 'result': 'SUCCESS' }
         
     def remove(self, battery_id):
-        for battery in self._batteries:
+        for battery in self._battery_array:
             if battery['_id'] == battery_id:
-                self._batteries.pop(self._batteries.index(battery))
+                self._battery_array.pop(self._battery_array.index(battery))
                 return { 'result': 'SUCCESS' }
         return { 'error': 'BATTERY_NOT_FOUND' }
         
     def connected_batteries(self):
         """Return all connected batteries."""
-        return self._batteries
+        return self._battery_array
         
-    def charge(self, charge_details: dict):
-        """"""
-        self._distribute_charge(charge_details)    
+    def charge(self, power: KiloWatt):
+        """Charge connected batteries, return state"""
+        self._distribute_charge(power)
     
-    def _distribute_charge(self, charge):
+    def _distribute_charge(self, power: KiloWatt):
         """Distribute charge equally amongst connected batteries."""
+        power_per_battery = power / len(self._battery_array)
+        [battery.charge(power_per_battery) for battery in self._battery_array]
         
     def get_battery_details(self):
-        battery_details = [battery.status() for battery in self._batteries]
-        avg_voltage = sum([battery['volts'] for battery in battery_details]) / len(battery_details)
+        battery_details = [battery.status() for battery in self._battery_array]
+        avg_voltage = sum([battery['available_power']['value'] for battery in battery_details]) / len(battery_details)
         return {
             'voltage': avg_voltage,
             'battery_details': battery_details
@@ -250,7 +238,6 @@ class PhotoVoltaicSystem:
         self._active: bool = False
         self._update_interval: int = 1
         self._time_series: List[dict] = []
-    
         
     def start(self):
         """Activate PV system."""
@@ -271,6 +258,7 @@ class PhotoVoltaicSystem:
     def _update(self):
         """Get current readings from solar and battery arrays."""
         while self._active:
+            self._charge_battery_array()        # send output from solar array to battery array
             panel_details = self._panels.get_panel_details()
             battery_details = self._batteries.get_battery_details()
             state = {
@@ -280,6 +268,18 @@ class PhotoVoltaicSystem:
             }
             self._time_series.append(state)
             time.sleep(self._update_interval)
+            
+    def _charge_battery_array(self):
+        panel_details = self._panels.get_panel_details()
+        self._batteries.charge(panel_details['total_output'])
+
+
+@freeze_time('May 21, 2024', auto_tick_seconds=1800)   # 1 second real time = 30 min sim time
+def simulated_time(environment: Environment):
+    """Updates an environment's time value based on specified interval."""
+    while True:
+        environment.set_time(datetime.now())
+        time.sleep(1)
 
 
 if __name__ == '__main__':
@@ -294,14 +294,16 @@ if __name__ == '__main__':
         'temp_coefficient': 0.11,
         'area': 3                  # meters squared
     })
-    battery = Battery(voltage=12, amps=50)
+    battery = Battery(capacity=12, amps=50)
     solar_array = SolarArray()
     battery_array = BatteryArray()
     solar_array.add(panel)
     battery_array.add(battery)
     system = PhotoVoltaicSystem(environment=environment, panels=solar_array, batteries=battery_array)
+    sim_time_thread = threading.Thread(target=simulated_time, args=(environment,))
+    sim_time_thread.start()
     environment.start()
     system.start()
     while True:
-        print(system.state())
+        print(json.dumps(system.state()))
         time.sleep(2)
